09f4fd0b013848d6c2085d4ce7a6e054fe3804e6
daryatash <daryatashkina23@gmail.com>
2025-12-22 10:18:30 +0300

    task-done

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,42 @@ clearButton.addEventListener('click', onClearInput)
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault()
 })
+
+const renderMeteoData = () => {
+    const cardsList = document.querySelector('.cards')
+    cardsList.innerHTML = ''
+
+    meteoData.forEach((card) => {
+        const cardsItem = document.createElement('li')
+
+        cardsItem.className = 'cards__item'
+        cardsItem.innerHTML = `
+            <h3 class="cards__item-title">${card.title}</h3>
+            <img src="${card.icon}" alt="" class="cards__item-icon">
+            <div class="cards__item-value">${card.value}</div>
+            <div class="cards__item-more">
+                ${card.more.progressBar 
+                    ? `<div class="cards__item-progress progress">
+                            <progress class="progress__bar ${card.title === 'Давление' ? 'progress__bar--pressure' : ''}" value="${card.more.progressValue}" max="${card.more.progressMax}">${card.value}</progress>
+                            <span class="progress__value" style="left: ${(card.more.progressValue / card.more.progressMax) * 100}%"></span>
+                        </div>`
+                    : ''
+                }
+                ${card.more.secondDescription 
+                    ? `<div class="cards__item-descriptions">
+                            <div>${card.more.firstDescription}</div>
+                            <div>${card.more.secondDescription ? card.more.secondDescription : ''}</div>
+                        </div>`
+                    : `<div class="cards__item-description">
+                            ${card.more.firstDescription}
+                        </div>`
+                }
+            </div>
+        `
+        cardsList.appendChild(cardsItem)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderMeteoData()
+})
